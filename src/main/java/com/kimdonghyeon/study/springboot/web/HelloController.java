ab60ae1320fabe2d0daf357b94274d1e86cb9c2a
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2020-11-10 22:54:34 +0900

    chapter 2 : add HelloController.java

--- src/main/java/com/kimdonghyeon/study/springboot/web/HelloController.java
+++ src/main/java/com/kimdonghyeon/study/springboot/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.kimdonghyeon.study.springboot.web;
 
+import com.kimdonghyeon.study.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController     // 컨트롤러를 JSON 타입 반환을 하도록 만들어 줌
@@ -10,5 +12,16 @@ public class HelloController {
     @GetMapping("/hello")       // Get(Http method) 요청을 받을수 있는 API 만들어 줌
     public String hello() {
         return "힘쎄고 강한 아침! 만일 내게 물어보면 나는 SpringBoot.";
+    }
+
+    // 롬복으로 처리되는 API 코드 추가
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount) {
+        /*
+        @RequestParam
+        외부에서 API한테 넘긴 파라미터를 가져오는 어노테이션
+        ```@RequestParam(전달받은_파라미터의_이름) 변수타입 전달받은_파라미터를_저장할_변수```
+         */
+        return new HelloResponseDto(name, amount);
     }
 }
--- src/test/java/com/kimdonghyeon/study/springboot/web/HelloControllerTest.java
+++ src/test/java/com/kimdonghyeon/study/springboot/web/HelloControllerTest.java
@@ -11,6 +11,9 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
 @RunWith(SpringRunner.class)
 /*
 테스트 진행시 JUnit 내장 실행자와는 다른 실행자 실행 (여기선 SpringRunner 라는 스프링 실행자 실행)
@@ -23,12 +26,29 @@ public class HelloControllerTest {      // HelloControllerTest 줄 왼쪽의 화
     private MockMvc mvc;    // 웹 API 테스트에 사용 -> 이 클래스로 HTTP GEt, POST 등에 대한 API 테스트 가능
 
     @Test
-    public void HelloResponseTest() throws Exception {
+    public void HelloTest() throws Exception {
         String hello = "힘쎄고 강한 아침! 만일 내게 물어보면 나는 SpringBoot.";
         // String hello = "안 힘쎄고 강한 아침! 만일 내게 물어보면 나는 SpringBoot.";     // 이걸로 테스트하면 테스트 실패 확인 가능
 
         mvc.perform(get("/hello"))      // MockMvc 를 통해 /hello 주소로 GET 요청을 보냄 (체이닝 지원 -> 검증 기능 여러개 같이 사용 가능)
                 .andExpect(status().isOk())     // mvc.perform 결과를 검증 (status가 OK(200) 인지)
                 .andExpect(content().string(hello));     // mvc.perform 결과를 검증 (content가 hello 변수 값과 같은지)
+    }
+
+
+    // 아래 코드 : 기존 코드를 롬복으로 리펙토링
+    @Test
+    public void HelloDtoTest() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(
+                get("/hello/dto")
+                .param("name", name)    // param() : API 테스트때 요청할 파라미터를 설정 (파라미터는 String 타입 값만 허용, 다른 타입도 반드시 String으로 변환해 사용)
+                .param("amount", String.valueOf(amount))
+            )
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.name", is(name)))    // jsonPath() : JSON 응답값을 필드별로 검증할 수 있는 메소드 ($를 기준으로 필드명 명시)
+            .andExpect(jsonPath("$.amount", is(amount)));
     }
 }
