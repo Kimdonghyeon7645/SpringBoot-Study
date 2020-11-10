123d4eeab4fbf1c73b2cc82d3772a313150ee91b
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2020-11-10 23:14:04 +0900

    chapter 2 : add gradle version issue url

--- src/test/java/com/kimdonghyeon/study/springboot/web/dto/HelloResponseDtoTest.java
+++ src/test/java/com/kimdonghyeon/study/springboot/web/dto/HelloResponseDtoTest.java
@@ -9,6 +9,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 2. 자동완성이 확실하게 지원
  */
 // fixed : Gradle 버전이 4.10.2 뒤라면, 테스트 실행시 에러난다. 그레들 버전을 다운그레이드 해주자. (gradlew wrapper --gradle-version 4.10.2)
+// readme : https://github.com/jojoldu/freelec-springboot2-webservice/issues/2
 
 public class HelloResponseDtoTest {
 
