91c9f325ef7000aadfcd3c41d599fc9535096f0f
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2020-11-15 23:32:47 +0900

    chapter 3 : add PostsRepositoryTest.java

---
+++ src/test/java/com/kimdonghyeon/study/springboot/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,46 @@
+package com.kimdonghyeon.study.springboot.domain.posts;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After      // Junit 에서 단위 테스트가 끝날때 마다 수행되는 메소드를 지정  
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void writeAndLoadPost() {
+        //given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("dongdongYee@gmail.com")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+}
