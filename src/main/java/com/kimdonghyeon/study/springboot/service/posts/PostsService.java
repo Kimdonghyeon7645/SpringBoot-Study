f181b32c4516ac015433af768c97828f3dab7d13
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2020-11-26 11:20:31 +0900

    chapter 3 : add Posts Controller, Service, Dto

---
+++ src/main/java/com/kimdonghyeon/study/springboot/service/posts/PostsService.java
@@ -0,0 +1,18 @@
+package com.kimdonghyeon.study.springboot.service.posts;
+
+import com.kimdonghyeon.study.springboot.domain.posts.PostsRepository;
+import com.kimdonghyeon.study.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
