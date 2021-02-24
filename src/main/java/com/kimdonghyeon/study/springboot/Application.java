abb5546ad6627becf9966a8b1ecc569cba35e638
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2021-02-24 21:26:27 +0900

    chapter 3 : activate JPA Auditing in Application.java

--- src/main/java/com/kimdonghyeon/study/springboot/Application.java
+++ src/main/java/com/kimdonghyeon/study/springboot/Application.java
@@ -1,7 +1,10 @@
 package com.kimdonghyeon.study.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+
+@EnableJpaAuditing  // JPA의 Auditing을 활성화
 @SpringBootApplication
 /*
 @SpringBootApplication
