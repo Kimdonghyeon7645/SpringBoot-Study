f7e5f26de53c4e75e7ec2613157459d71ebf5b1a
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2021-02-26 16:48:48 +0900

    chapter 4 : add comment in index.mustache, IndexController.java

--- src/main/java/com/kimdonghyeon/study/springboot/web/IndexController.java
+++ src/main/java/com/kimdonghyeon/study/springboot/web/IndexController.java
@@ -9,5 +9,7 @@ public class IndexController {
     @GetMapping("/")
     public String index() {
         return "index";
+        // 머스테치 스타터 덕분에 컨트롤러에서 문자열 반환시, 앞의 경로(src/main/resources/templates) + 뒤의 파일 확장자(.mustache)는 자동으로 지정
+        // ViewResolver(URL 요청의 결과를 전달할 타입, 값을 지정하는 관리자 같은 느낌)가 위같이 자동으로 지정된 값으로 처리함
     }
 }
