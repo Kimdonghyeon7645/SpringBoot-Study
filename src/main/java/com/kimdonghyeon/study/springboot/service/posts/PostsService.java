6c61978b522635d73d354913324721c19aa7d88b
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2021-03-01 23:09:29 +0900

    chapter 4 : add comment in PostsService.java

--- src/main/java/com/kimdonghyeon/study/springboot/service/posts/PostsService.java
+++ src/main/java/com/kimdonghyeon/study/springboot/service/posts/PostsService.java
@@ -41,6 +41,7 @@ public class PostsService {
     }
 
     @Transactional(readOnly = true)
+    // readOnly = true : 트렌젝션 범위는 유지하되, 조회 기능만 남겨둠 -> 조회 속도 개선 (등록, 수정, 삭제 기능이 전혀없는 서비스 메소드에서 사용하는 것을 추천)
     public List<PostsListResponseDto> findAllDesc() {
         return postsRepository.findAllDesc().stream()
                 .map(PostsListResponseDto::new)
