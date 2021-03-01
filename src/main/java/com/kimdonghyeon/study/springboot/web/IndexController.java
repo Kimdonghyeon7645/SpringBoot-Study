0c74ee6a2262d78eb8f9645bdcb963f11625708e
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2021-03-01 23:33:08 +0900

    chapter 4 : add post-update part

--- src/main/java/com/kimdonghyeon/study/springboot/web/IndexController.java
+++ src/main/java/com/kimdonghyeon/study/springboot/web/IndexController.java
@@ -1,10 +1,13 @@
 package com.kimdonghyeon.study.springboot.web;
 
 import com.kimdonghyeon.study.springboot.service.posts.PostsService;
+import com.kimdonghyeon.study.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RequiredArgsConstructor
 @Controller
@@ -29,5 +32,13 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
     }
 }
