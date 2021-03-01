7ae7a30b5a832bd23996c041a78528937d87f689
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2021-03-01 14:44:52 +0900

    chapter 4 : add post-save part

--- src/main/java/com/kimdonghyeon/study/springboot/web/IndexController.java
+++ src/main/java/com/kimdonghyeon/study/springboot/web/IndexController.java
@@ -12,4 +12,9 @@ public class IndexController {
         // 머스테치 스타터 덕분에 컨트롤러에서 문자열 반환시, 앞의 경로(src/main/resources/templates) + 뒤의 파일 확장자(.mustache)는 자동으로 지정
         // ViewResolver(URL 요청의 결과를 전달할 타입, 값을 지정하는 관리자 같은 느낌)가 위같이 자동으로 지정된 값으로 처리함
     }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
+    }
 }
