261e0b916568760f38377caf934aa0692d90c9b1
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2021-03-04 19:29:23 +0900

    chapter 5 : add comment in UserRepository.java

--- src/main/java/com/kimdonghyeon/study/springboot/domain/user/UserRepository.java
+++ src/main/java/com/kimdonghyeon/study/springboot/domain/user/UserRepository.java
@@ -10,5 +10,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     /*
      소셜 로그인으로 반환되는 값의 email을 통하여
      이미 생성된 사용자인지 처음 가입하는 사용자인지 판단하기 위한 메소드
+
+     Optional<T> 란 : https://www.daleseo.com/java8-optional-after/
      */
 }
