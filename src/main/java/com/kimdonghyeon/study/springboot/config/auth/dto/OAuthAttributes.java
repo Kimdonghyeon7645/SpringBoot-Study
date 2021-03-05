7b6a34a368b2b32b1ab8c55328dd143dda768bba
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2021-03-05 19:13:50 +0900

    chapter 5 : add naver_login in OAuthAttributes.java

--- src/main/java/com/kimdonghyeon/study/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/kimdonghyeon/study/springboot/config/auth/dto/OAuthAttributes.java
@@ -25,10 +25,13 @@ public class OAuthAttributes {
     }
 
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
-        return ofGoogle(userNameAttributeName, attributes);
+        if("naver".equals(registrationId)) {        // 네이버 로그인
+            return ofNaver("id", attributes);
+        }
+        return ofGoogle(userNameAttributeName, attributes);     // 구글 로그인
     }
 
-    public static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
+    private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
         return OAuthAttributes.builder()
                 .name((String) attributes.get("name"))
                 .email((String) attributes.get("email"))
@@ -36,10 +39,22 @@ public class OAuthAttributes {
                 .attributes(attributes)
                 .nameAttributeKey(userNameAttributeName)
                 .build();
+        /*
+         OAuth2User가 반환하는 사용자 정보가 Map(attributes)이라, 값 하나하나를 변환해야 함
+         */
     }
-    /*
-     OAuth2User가 반환하는 사용자 정보가 Map(attributes)이라, 값 하나하나를 변환해야 함
-     */
+
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
 
     public User toEntity() {
         return User.builder()
