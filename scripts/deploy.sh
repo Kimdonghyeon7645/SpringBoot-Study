71c26e11d0e8e20b05f382ee96a73524647eb3aa
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2021-03-18 16:02:11 +0900

    fix : edit deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -30,7 +30,6 @@ JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)    # /step2에서 수정
 echo ">> JAR 파일명 : $JAR_NAME"
 
 echo ">> $JAR_NAME 에 실행권한 추가"
-echo "^^ $(ls)}"
 chmod +x $JAR_NAME    # 여기서 Jar 파일은 실행권한이 없는 상태이므로, nohup으로 실행할 수 있는 권한 부여
 
 nohup java -jar \
